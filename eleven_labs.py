db6a7b37b4e25d4b9761960d2b800038dc35c86a
Alex Boisvert <boisvert42@gmail.com>
2024-05-11 16:48:05 -0700

    Make elevenlabs work for me

--- eleven_labs.py
+++ eleven_labs.py
@@ -1,21 +1,26 @@
 from elevenlabs import generate, stream, set_api_key, voices, play, save
 import time
 import os
+import configparser
 
-try:
-  set_api_key(os.getenv('ELEVENLABS_API_KEY'))
-except TypeError:
-  exit("Ooops! You forgot to set ELEVENLABS_API_KEY in your environment!")
+# Set up secrets
+_config = configparser.ConfigParser()
+_pth = os.path.join(os.path.expanduser('~'), '.api_keys')
+_config.read(_pth)
+ELEVENLABS_API_KEY = _config['ElevenLabs']['API_KEY']
+set_api_key(ELEVENLABS_API_KEY)
+
+DEFAULT_VOICE = 'Dorothy'
 
 class ElevenLabsManager:
 
     def __init__(self):
         # CALLING voices() IS NECESSARY TO INSTANTIATE 11LABS FOR SOME FUCKING REASON
         all_voices = voices()
-        print(f"\nAll ElevenLabs voices: \n{all_voices}\n")
+        #print(f"\nAll ElevenLabs voices: \n{all_voices}\n")
 
     # Convert text to speech, then save it to file. Returns the file path
-    def text_to_audio(self, input_text, voice="Doug VO Only", save_as_wave=True, subdirectory=""):
+    def text_to_audio(self, input_text, voice=DEFAULT_VOICE, save_as_wave=True, subdirectory=""):
         audio_saved = generate(
           text=input_text,
           voice=voice,
@@ -30,7 +35,7 @@ class ElevenLabsManager:
         return tts_file
 
     # Convert text to speech, then play it out loud
-    def text_to_audio_played(self, input_text, voice="Doug VO Only"):
+    def text_to_audio_played(self, input_text, voice=DEFAULT_VOICE):
         audio = generate(
           text=input_text,
           voice=voice,
@@ -39,7 +44,7 @@ class ElevenLabsManager:
         play(audio)
 
     # Convert text to speech, then stream it out loud (don't need to wait for full speech to finish)
-    def text_to_audio_streamed(self, input_text, voice="Doug VO Only"):
+    def text_to_audio_streamed(self, input_text, voice=DEFAULT_VOICE):
         audio_stream = generate(
           text=input_text,
           voice=voice,
@@ -52,11 +57,11 @@ class ElevenLabsManager:
 if __name__ == '__main__':
     elevenlabs_manager = ElevenLabsManager()
 
-    elevenlabs_manager.text_to_audio_streamed("This is my streamed test audio, I'm so much cooler than played", "Doug Melina")
+    #elevenlabs_manager.text_to_audio_streamed("This is my streamed test audio, I'm so much cooler than played", "Dorothy")
+    #time.sleep(2)
+    elevenlabs_manager.text_to_audio_played("This is my played test audio, hello hello", DEFAULT_VOICE)
     time.sleep(2)
-    elevenlabs_manager.text_to_audio_played("This is my played test audio, helo hello", "Doug Melina")
-    time.sleep(2)
-    file_path = elevenlabs_manager.text_to_audio("This is my saved test audio, please make me beautiful", "Doug Melina")
+    file_path = elevenlabs_manager.text_to_audio("This is my saved test audio, please make me beautiful", DEFAULT_VOICE)
     print("Finished with all tests")
 
     time.sleep(30)
